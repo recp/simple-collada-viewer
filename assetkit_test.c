3e7a7dadbf1ffb003843e7e9fd415422e0da613b
Recep Aslantas <info@recp.me>
2017-02-16 00:33:21 +0300

    add documentation/comments

--- assetkit_test.c
+++ assetkit_test.c
@@ -36,7 +36,7 @@ load_collada() {
   /* render context */
   ctx = gkContextNew();
 
-  /* load whole visual scene returns GkScene for rendering */
+  /* load whole visual scene, this returns GkScene for rendering */
   agk_loadScene(ctx,
                 doc,
                 &doc->scene,
@@ -58,14 +58,24 @@ init() {
   glm_mat4_inv(viewMatrix, scene->v);
 
   /* default shaders: common materials */
-  /* NOTE: every node, model can have difeerent shaders e.g. GLSL profile  */
-  /*       if nodes, models don't have program info then they use paren's. */
-  /*       so we provided only ONE prog info so all nodes will use this    */
+  /* NOTE: every node, model can have different shaders e.g. GLSL profile  */
+  /*       if nodes, models don't have program info then they use parent's */
+  /*       so we provided only ONE prog info and all nodes will use this   */
+  /*       also check bottom to see how to use custom shaders              */
   scene->pinfo    = gkDefaultProgram();
 
   /* invalidate VIEW matrix
        = force re-render + re-calculate and cache model and final matrices  */
   scene->vIsValid = 0;
+
+  /* NOTE for node matrices: all matrices will be cached by first draw call */
+  /*      so you don't need manage cache manually BUT YOU CAN INVALIDATE    */
+  /*      if you invalidate matrix, then all sub-nodes' matrices will be    */
+  /*      recalculated (only children and instance nodes) and cached again  */
+  /*      also like prog info, nodes, models use parent matrix if they      */
+  /*      don't have any transform, this saves extra space, for instance;   */
+  /*      if only root node has transform then all sub-nodes, node-instances*/
+  /*      will use same matrix, space=1 * sizeof(mat4) * ptrsize * refcount */
 
   glEnable(GL_DEPTH_TEST);
   glClearColor(0.64, 0.64, 0.64, 1);
@@ -133,10 +143,12 @@ dealloc() {
    gkAttachShaders(program, shaders);
    glLinkProgram(program);
 
+#ifdef DEBUG
    if (!gkProgramIsValid(program)) {
      gkProgramLogInfo(program, stderr);
      exit(-1);
    }
+#endif
 
    glUseProgram(program);
 
